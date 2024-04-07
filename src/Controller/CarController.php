1d673289ee7794b05940d7d9bee71ea20695a273
Yoru-cyber <carlosmendez170210@gmail.com>
2024-04-07 11:04:17 -0400

    added DI to CarController

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -10,22 +10,22 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class CarController extends AbstractController
 {
-    // private CarRepository $car_repository;
-    // public function __construct(CarRepository $repository)
-    // {
-    //     $this->car_repository = $repository;
-    // }
+    private EntityManagerInterface $entityManager;
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
 
     #[Route('/cars', name: 'app_cars')]
-    public function index(EntityManagerInterface $entityManager): JsonResponse
+    public function index(): JsonResponse
     {
-        return $this->json($entityManager->getRepository(Car::class)->findAll());
+        return $this->json($this->entityManager->getRepository(Car::class)->findAll());
     }
 
     #[Route('/cars/{id}', name: 'app_car_by_id')]
-    public function getbyID(EntityManagerInterface $entityManager, int $id): JsonResponse
+    public function getbyID(int $id): JsonResponse
     {
-        $car = $entityManager->getRepository(Car::class)->find($id);
+        $car = $this->entityManager->getRepository(Car::class)->find($id);
         if ($car != null) {
             return $this->json($car);
         }
