ea7f399fd356008ba7a48386181bbaf30d3ee450
Yoru-cyber <carlosmendez170210@gmail.com>
2024-04-07 03:03:10 -0400

    added get a car by id and error handling

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -16,9 +16,19 @@ class CarController extends AbstractController
     //     $this->car_repository = $repository;
     // }
 
-    #[Route('/cars', name: 'app_car')]
+    #[Route('/cars', name: 'app_cars')]
     public function index(EntityManagerInterface $entityManager): JsonResponse
     {
         return $this->json($entityManager->getRepository(Car::class)->findAll());
+    }
+
+    #[Route('/cars/{id}', name: 'app_car_by_id')]
+    public function getbyID(EntityManagerInterface $entityManager, int $id): JsonResponse
+    {
+        $car = $entityManager->getRepository(Car::class)->find($id);
+        if ($car != null) {
+            return $this->json($car);
+        }
+        return $this->json(['Error' => 'element not found'], 404);
     }
 }
